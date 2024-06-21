ff635f08cb4e3e66c47e88057dd002610ca8f827
Ashmit200 <ashmittyagi8@gmail.com>
2024-06-21 10:48:34 +0530

    Implement the Complete Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+
+import RightSidebar from '@/components/RightSidebar';
+import HeaderBox from '../../components/HeaderBox'
+import TotalBalanceBox from '../../components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = {firstName:'Ashmit',lastName:'Tyagi',email:'ashmittyagi8@gmail.com'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently."
+          />
+        <TotalBalanceBox
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.35}
+        
+        />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance:123.50} , {currentBalance:234.46}]}
+      />
+    </section>
   )
 }
 
---
+++ components/SideBar.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import Image from "next/image"
+import Link from "next/link"
+import { sidebarLinks } from "../constants"
+import { cn } from "../lib/utils"
+import { usePathname } from "next/navigation"
+
+const SideBar = ({user}:  SiderbarProps) => {
+  const Pathname = usePathname();
+  return (
+    <section className='sidebar'>
+    <nav className='flex flex-col gap-4'>
+      <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+      <Image 
+      src="/icons/logo.svg"
+      width={34}
+      height={34}
+      alt="Murli Dutt Logo"
+      className="size-[24px] max-xl:size-14"/>
+      <h1 className="sidebar-logo">MurliDutt</h1>
+      </Link>
+
+      {sidebarLinks.map((item) => {
+        const isActive =  Pathname === item.route || Pathname.startsWith(`${item.route}/`)
+      return(
+        <Link
+        href={item.route} key={item.label}
+        className={cn('sidebar-link',{
+          'bg-bank-gradient': isActive}
+        )}>
+        <div className="relative size-6">
+          <Image 
+          src={item.imgURL}
+          alt={item.label}
+          fill
+          className={cn({
+            'brightness-[3] invert-0':isActive
+          })}
+          />
+        </div>
+        <p className={cn(
+          'sidebar-label',
+          {'!text-white' : isActive}
+        )}> {item.label}</p>
+        </Link>
+      )
+    })}
+
+    
+    </nav>
+    </section>
+  )
+}
+
+export default SideBar
